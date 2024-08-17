303499285b6776c56b27d547dc2dc5171e2dceec
ddeiro <ddeiro@bdsdatasolution.com.br>
2024-08-17 09:36:03 -0300

    build: Exercicio 02 aplicado.

---
+++ aula_01/exercicio_02.py
@@ -0,0 +1,6 @@
+# Crie um programa que o usuário digite dois valores e apareça a soma deles.
+
+valor_01 = int(input('Digite o primeiro valor: '))
+valor_02 = int(input('Digite o segundo valor: '))
+soma = valor_01 + valor_02
+print(f'A soma dos valores {valor_01} e {valor_02} é: {soma}')
